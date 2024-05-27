5355377313931eb5436e69576af54e5e93cc6ea0
Bin Bin <phuduthang300502@gmail.com>
2024-05-27 20:52:57 +0700

    CLEAN UP ADDRESS FIELD

--- src/components/customs/AddressField.jsx
+++ src/components/customs/AddressField.jsx
@@ -7,100 +7,79 @@ const URL_PROVINCE = "https://vapi.vnappmob.com/api/province/";
 const URL_DISTRICT = "https://vapi.vnappmob.com/api/province/district/";
 const URL_WARD = "https://vapi.vnappmob.com/api/province/ward/";
 
+const AddressField = ({ data, props }) => {
+	return (
+		<TextField {...props}>
+			{data.map((option) => (
+				<MenuItem
+					key={option.id}
+					value={AddressUtil.hash(option.id, option.title)}
+					sx={{ padding: "10px", fontStyle: "italic" }}
+				>
+					{option.title}
+				</MenuItem>
+			))}
+		</TextField>
+	);
+};
+
 export const ProvinceField = (props) => {
-	const [PROVINCES, setProvinces] = useState([]);
+	const [data, setData] = useState([]);
 	useEffect(() => {
 		fetch(URL_PROVINCE)
 			.then((respone) => respone.json())
 			.then((data) =>
 				data.results
-					.map((province) => ({
-						id: province.province_id,
-						title: province.province_name,
-					}))
+					.map((e) => ({ id: e.province_id, title: e.province_name }))
 					.sort((a, b) => a.title.localeCompare(b.title))
 			)
-			.then((results) => setProvinces(results))
+			.then((results) => setData(results))
 			.catch((error) => console.error("ERROR FETCHING DATA:", error));
 	}, []);
 
-	return (
-		<TextField {...props}>
-			{PROVINCES.map((option) => (
-				<MenuItem
-					key={option.id}
-					value={AddressUtil.hash(option.id, option.title)}
-					sx={{ padding: "10px", fontStyle: "italic" }}
-				>
-					{option.title}
-				</MenuItem>
-			))}
-		</TextField>
-	);
+	return <AddressField data={data} props={props} />;
 };
 
 export const DistrictField = ({ selectedProvince, ...props }) => {
-	const [DISTRICTS, setDistricts] = useState([]);
+	const [data, setData] = useState([]);
+	const isActived = Boolean(selectedProvince);
+
 	useEffect(() => {
-		if (!Boolean(selectedProvince)) return;
+		if (!isActived) return;
 		fetch(URL_DISTRICT + AddressUtil.getKey(selectedProvince))
 			.then((respone) => respone.json())
 			.then((data) =>
 				data.results
-					.map((district) => ({
-						id: district.district_id,
-						title: district.district_name,
-					}))
+					.map((e) => ({ id: e.district_id, title: e.district_name }))
 					.sort((a, b) => a.title.localeCompare(b.title))
 			)
-			.then((results) => setDistricts(results))
+			.then((results) => setData(results))
 			.catch((error) => console.error("ERROR FETCHING DATA:", error));
 	}, [selectedProvince]);
 
 	return (
-		<TextField {...props} disabled={!Boolean(selectedProvince)}>
-			{DISTRICTS.map((option) => (
-				<MenuItem
-					key={option.id}
-					value={AddressUtil.hash(option.id, option.title)}
-					sx={{ padding: "10px", fontStyle: "italic" }}
-				>
-					{option.title}
-				</MenuItem>
-			))}
-		</TextField>
+		<AddressField data={data} props={{ ...props, disabled: !isActived }} />
 	);
 };
 
 export const WardField = ({ selectedDistrict, ...props }) => {
-	const [WARDS, setWards] = useState([]);
+	const [data, setData] = useState([]);
+	const isActived = Boolean(selectedDistrict);
+
 	useEffect(() => {
-		if (!Boolean(selectedDistrict)) return;
+		if (!isActived) return;
 		fetch(URL_WARD + AddressUtil.getKey(selectedDistrict))
 			.then((respone) => respone.json())
 			.then((data) =>
 				data.results
-					.map((ward) => ({
-						id: ward.ward_id,
-						title: ward.ward_name,
-					}))
+					.map((e) => ({ id: e.ward_id, title: e.ward_name }))
 					.sort((a, b) => a.title.localeCompare(b.title))
 			)
-			.then((results) => setWards(results))
+			.then((results) => setData(results))
 			.catch((error) => console.error("ERROR FETCHING DATA:", error));
 	}, [selectedDistrict]);
 
 	return (
-		<TextField {...props} disabled={!Boolean(selectedDistrict)}>
-			{WARDS.map((option) => (
-				<MenuItem
-					key={option.id}
-					value={AddressUtil.hash(option.id, option.title)}
-					sx={{ padding: "10px", fontStyle: "italic" }}
-				>
-					{option.title}
-				</MenuItem>
-			))}
-		</TextField>
+		<AddressField data={data} props={{ ...props, disabled: !isActived }} />
 	);
 };
