33fb820d7f0122ad60d69ad42c6a2e66bdab2e96
Bin Bin <phuduthang300502@gmail.com>
2024-06-04 17:55:40 +0700

    SINGLETON API

--- src/api/api.js
+++ src/api/api.js
@@ -1,36 +1,53 @@
 import axios from "axios";
-import { ACCESS_TOKEN, BASE_URL, REFRESH_TOKEN } from "./constants";
+import { ACCESS_TOKEN, BASE_URL } from "./constants";
 
-const AxiosInstance = axios.create({
-	baseURL: BASE_URL,
-	timeout: 5000,
-	headers: {
-		"Content-Type": "application/json",
-		accept: "application/json",
-	},
-});
+class APISingleton {
+	static #instance;
+	#axiosInstance;
 
-AxiosInstance.interceptors.request.use(
-	(config) => {
-		const token = localStorage.getItem(ACCESS_TOKEN);
-		// if (token) config.headers.Authorization = `Bearer ${token}`;
-		if (token) config.headers.Authorization = `Token ${token}`;
-		return config;
-	},
-	(error) => Promise.reject(error)
-);
-
-AxiosInstance.interceptors.response.use(
-	(response) => {
-		return response;
-	},
-	(error) => {
-		if (error.response && error.response.status === 401) {
-			localStorage.removeItem(ACCESS_TOKEN);
-			// localStorage.removeItem(REFRESH_TOKEN);
+	constructor() {
+		if (APISingleton.#instance === undefined) {
+			// CREATE AXIOS INSTANCE.
+			this.#axiosInstance = axios.create({
+				baseURL: BASE_URL,
+				timeout: 5000,
+				headers: {
+					"Content-Type": "application/json",
+					accept: "application/json",
+				},
+			});
+			// REQUEST CONFIGURATION.
+			this.#axiosInstance.interceptors.request.use(
+				(config) => {
+					const token = localStorage.getItem(ACCESS_TOKEN);
+					if (token) config.headers.Authorization = `Token ${token}`;
+					return config;
+				},
+				(error) => Promise.reject(error)
+			);
+			// RESPONSE CONFIGURATION.
+			this.#axiosInstance.interceptors.response.use(
+				(response) => {
+					return response;
+				},
+				(error) => {
+					if (error.response && error.response.status === 401) {
+						localStorage.clear();
+					}
+					return Promise.reject(error);
+				}
+			);
+			// STORE INSTANCE.
+			APISingleton.#instance = this;
 		}
-		return Promise.reject(error);
+		return APISingleton.#instance;
 	}
-);
+
+	getAxiosInstance() {
+		return this.#axiosInstance;
+	}
+}
+
+const AxiosInstance = new APISingleton().getAxiosInstance();
 
 export default AxiosInstance;
