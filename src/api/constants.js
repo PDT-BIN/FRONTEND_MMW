59b7d3c74dbaff288d72c2709a720a3773933877
Bin Bin <phuduthang300502@gmail.com>
2024-06-04 18:20:35 +0700

    FIX PROTECTED ROUTE

--- src/api/constants.js
+++ src/api/constants.js
@@ -2,8 +2,6 @@ export const BASE_URL = "http://127.0.0.1:8000/";
 
 export const ACCESS_TOKEN = "access";
 
-export const REFRESH_TOKEN = "refresh";
-
 export const PROFILE_ID = "profile_id";
 
 export const IS_MANAGER = "is_superuser";
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,47 +1,17 @@
-import { jwtDecode } from "jwt-decode";
 import { useState, useEffect } from "react";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 import LoadingIndicator from "./LoadingIndicator.jsx";
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "../api/constants.js";
-import AxiosInstance from "../api/api.js";
+import { ACCESS_TOKEN } from "../api/constants.js";
 
 function ProtectedRoute() {
 	// AUTHENTICATE USER.
+	const location = useLocation();
 	const [isAuthenticated, setIsAuthenticated] = useState(null);
+
 	useEffect(() => {
-		authenticate().catch(() => setIsAuthenticated(false));
-	}, []);
-
-	// const refreshToken = async () => {
-	// 	const token = localStorage.getItem(REFRESH_TOKEN);
-	// 	if (!token) {
-	// 		setIsAuthenticated(false);
-	// 		return;
-	// 	}
-	// 	// REFRESH TOKEN.
-	// 	const respone = await AxiosInstance.post("/api/token/refresh/", {
-	// 		refresh: token,
-	// 	});
-	// 	// CHECK RESPONE.
-	// 	if (respone.status !== 200) setIsAuthenticated(false);
-	// 	else {
-	// 		localStorage.setItem(ACCESS_TOKEN, respone.data.access);
-	// 		setIsAuthenticated(true);
-	// 	}
-	// };
-
-	const authenticate = async () => {
 		const token = localStorage.getItem(ACCESS_TOKEN);
-		if (!token) {
-			setIsAuthenticated(false);
-			return;
-		}
-		// CHECK TOKEN.
-		// const decoded = jwtDecode(token);
-		// if (decoded.exp < Date.now() / 1000) await refreshToken();
-		// else setIsAuthenticated(true);
-		setIsAuthenticated(true);
-	};
+		setIsAuthenticated(token !== null);
+	}, [location]);
 
 	switch (isAuthenticated) {
 		case null:
